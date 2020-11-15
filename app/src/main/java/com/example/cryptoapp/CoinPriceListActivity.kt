17012448d3a3225876f42ddf674449a79d774cf2
Anton Krasnov <Anton_oren@bk.ru>
2020-11-16 01:35:17 +0500

    add adapter

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.cryptoapp.adapters.CoinInfoAdapter
+import com.example.cryptoapp.pojo.CoinPriceInfo
+import kotlinx.android.synthetic.main.activity_coin_price_list.*
 
 class CoinPriceListActivity : AppCompatActivity() {
 
@@ -14,14 +17,17 @@ class CoinPriceListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coin_price_list)
 
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                Log.d("ON_CLICK_TEST", coinPriceInfo.fromSymbol)
+            }
+        }
+        rvCoinPriceList.adapter = adapter
+
         viewModel = ViewModelProviders.of(this)[CoinViewModel::class.java]
-
-
-//        viewModel.priceList.observe(this, Observer {
-//            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
-//        })
-        viewModel.getDetailInfo("BTC").observe(this, Observer {
-            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
         })
     }
 
